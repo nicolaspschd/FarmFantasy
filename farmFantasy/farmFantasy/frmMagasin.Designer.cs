23c841b12c857df77f0de459c785d8e02b63691b
Unknown <ardi.rmsh@eduge.ch>
2016-10-04 09:28:10 +0200

    design

--- farmFantasy/farmFantasy/frmMagasin.Designer.cs
+++ farmFantasy/farmFantasy/frmMagasin.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmMagasin));
             this.label1 = new System.Windows.Forms.Label();
             this.gbxSemence = new System.Windows.Forms.GroupBox();
             this.lblTotalSem = new System.Windows.Forms.Label();
@@ -475,6 +476,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(481, 386);
             this.Controls.Add(this.lblArgentMagas);
             this.Controls.Add(this.gbxProd);
