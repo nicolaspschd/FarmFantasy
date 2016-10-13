13b18379df47af3f9f64e30a3eca1123579f5a15
nicolaspschd <nicolas.pschd@eduge.ch>
2016-10-13 09:37:06 +0200

    Page sql

--- farmFantasy/farmFantasy/frmMagasin.cs
+++ farmFantasy/farmFantasy/frmMagasin.cs
@@ -7,8 +7,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-//using System.Data.Sql;
-
 namespace farmFantasy
 {
     public partial class frmMagasin : Form
---
+++ farmFantasy/farmFantasy/sql.cs
@@ -0,0 +1,31 @@
+﻿/*
+ * Auteurs : RAMUSHI Ardi && PASCHOUD Nicolas
+ * Nom du programme : Farm Fantasy
+ * Description : FarmFantasy est un jeu de gestion de ferme évolutif. 
+ *               Plusieurs animaux et type de cultures seront 
+ *               disponible ainsi que des améliorations de bâtiments.
+ * Date : 1 Septembre 2016
+ * Version 1.0
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace farmFantasy
+{
+    public partial class frmMain : Form
+    {
+        static string infoDB = "server=127.0.0.1;user=root;database=farmfantasy;password=;";
+        MySqlConnection connectionDB = new MySqlConnection();
+        MySqlCommand cmd;
+        public void Sauvegarder()
+        {
+
+        }
+    }
+}
